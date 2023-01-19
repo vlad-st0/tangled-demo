b2305f5d43606bb85d8af1e3d1d5206c4b60bd14
vladimir <vladsto17@gmail.com>
2023-01-20 00:25:20 +0100

    sh script space and variable wrapping

--- api/docker-entrypoint.dev.sh
+++ api/docker-entrypoint.dev.sh
@@ -2,17 +2,16 @@
 
 npm i -g tsc-watch
 yarn install
-
-echo "waiting for ${MIKRO_ORM_HOST}:${MIKRO_ORM_PORT}"
-
+echo "waiting for \"${MIKRO_ORM_HOST}\":\"${MIKRO_ORM_PORT}\""
 cycle=0
-while ! nc -z ${MIKRO_ORM_HOST} ${MIKRO_ORM_PORT}; do
+while ! nc -z "${MIKRO_ORM_HOST}" "${MIKRO_ORM_PORT}"; do
     sleep 1
     cycle=$((cycle + 1))
-    if [ "$cycle" -ge 60 ]; then echo "timeout"; exit 1; fi
+    if [ "$cycle" -ge 60 ]; then
+        echo "timeout"
+        exit 1
+    fi
 done
 echo "db online"
-
 yarn mikro
 yarn dev
-
--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 yarn install
-yarn tsc 
-
-echo "waiting for ${MIKRO_ORM_HOST}:${MIKRO_ORM_PORT}"
-
+yarn tsc
+echo "waiting for \"${MIKRO_ORM_HOST}\":\"${MIKRO_ORM_PORT}\""
 cycle=0
-while ! nc -z ${MIKRO_ORM_HOST} ${MIKRO_ORM_PORT}; do
+while ! nc -z "${MIKRO_ORM_HOST}" "${MIKRO_ORM_PORT}"; do
     sleep 1
     cycle=$((cycle + 1))
-    if [ "$cycle" -ge 60 ]; then echo "timeout"; exit 1; fi
+    if [ "$cycle" -ge 60 ]; then
+        echo "timeout"
+        exit 1
+    fi
 done
 echo "db online"
-
 yarn mikro
-yarn prod 
-
+yarn prod
